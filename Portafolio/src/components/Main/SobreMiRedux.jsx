51935a0ceaed09f1078f375186fde42d20efb14d
zuri <zurimanes@gmail.com>
2024-09-23 23:28:34 +0200

    Agrego animación a "sobre mi redux"

--- Portafolio/src/components/Main/SobreMiRedux.jsx
+++ Portafolio/src/components/Main/SobreMiRedux.jsx
@@ -1,9 +1,38 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./SobreMiRedux.css";
 
 function SobreMiRedux() {
+    const [nubePosX, setNubePosX] = useState(50);
+
+    useEffect(() => {
+        const handleMouseMove = (e) => {
+            const mouseX = e.clientX;
+            const screenWidth = window.innerWidth;
+
+            const centerX = screenWidth / 2;
+            const maxShift = 50;
+
+            let offset = ((mouseX - centerX) / centerX) * maxShift;
+
+            let newPosX = 50 + offset;
+
+            newPosX = Math.max(35, Math.min(newPosX, 65));
+
+            setNubePosX(newPosX);
+        };
+
+        window.addEventListener("mousemove", handleMouseMove);
+
+        return () => {
+            window.removeEventListener("mousemove", handleMouseMove);
+        };
+    }, []);
+
     return (
-        <div className="contenedorSobreMi">
+        <div
+            className="contenedorSobreMi"
+            style={{ transform: `translateX(${nubePosX - 50}%)` }}
+        >
             <section className="seccionSobreMi">
                 <p>
                     Soy un desarrollador web que pasa su tiempo libre
